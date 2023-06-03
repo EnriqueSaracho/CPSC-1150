078f83c48abb5cbcea8b83a6c6df881809879a8c
Enrique Saracho Felix <esarachof97@gmail.com>
2023-06-02 17:51:15 -0700

    week 5 notes and samples creation

--- notes/Week5.java
+++ notes/Week5.java
@@ -1,18 +1,70 @@
 package notes;
 
 // import java.lang.Math;
+import java.util.Scanner;
+import java.lang.String;
 
 public class Week5 {
     public static void main(String[] args) {
         // double random = Math.random() * (77 - -93) + -93;
         // System.out.println(random);
 
-        int x;
-        x = (int) (Math.random() * 999 + 1);
-        if (x % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
-        }
+        // int x;
+        // x = (int) (Math.random() * 999 + 1);
+        // if (x % 2 == 0) {
+        // System.out.println("Even");
+        // } else {
+        // System.out.println("Odd");
+        // }
+
+        // Notes: {Switch statement}
+        // int year;
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter year: ");
+        // year = input.nextInt();
+        // switch (year) {
+        // case 1:
+        // System.out.println("Freshman");
+        // break;
+        // case 2:
+        // System.out.println("Sophomore");
+        // break;
+        // case 3:
+        // System.out.println("Junior");
+        // break;
+        // case 4:
+        // System.out.println("Senior");
+        // break;
+        // default:
+        // System.out.println("Invalid year");
+        // }
+        // input.close();
+
+        // Notes: {String: double quotes within double quotes, and Backslash "\"}
+        // String sentence = "You said \"They are Friends\" \nI guess!";
+        // System.out.println(sentence);
+
+        // Notes: {Char, Unicode}
+        // char letter = 'A';
+        // char number = '4';
+        // char letterUnicode = '\u0041';
+        // System.out.println("\u03b1 \u03b2 \u03b3 " + letter + " " + number + " " +
+        // letterUnicode);
+
+        // char myChar1 = 0x0041;
+        // char myChar2 = (char) 0xD40041;
+        // char myChar3 = (char) 65.143;
+        // System.out.println(myChar1);
+        // System.out.println(myChar2);
+        // System.out.println(myChar3);
+
+        // Example: {triming text program.}
+        // String text;
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter a text: ");
+        // text = input.nextLine();
+        // int before = text.length();
+        // String = System.out.println(text);
+        // input.close();
     }
 }
---
+++ sample.java
@@ -0,0 +1,12 @@
+/**
+ * Program name:
+ * (program description/purpose)
+ * 
+ * @author Enrique Saracho Felix
+ * @since June 2, 2023
+ * 
+ */
+
+public class sample {
+
+}
