3c6fcbf10bce17381b84eb16c90d6b07bc9ff413
Enrique Saracho Felix <esarachof97@gmail.com>
2023-06-02 19:20:36 -0700

    created Sort3.java
    lab04 Q1

---
+++ lab04/Sort3.java
@@ -0,0 +1,19 @@
+package lab04;
+
+/**
+ * Program name:
+ * (program description/purpose)
+ * 
+ * @author Enrique Saracho Felix
+ * @since June 2, 2023
+ * 
+ */
+public class Sort3 {
+    /**
+     * main: takes three integer values as input, sorts them in ascending order and
+     * displays them.
+     */
+    public static void main(String[] args) {
+
+    }
+}
