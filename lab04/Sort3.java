bc520ccf49ef54c1b7ed339cccbe345beb30da7f
Enrique Saracho Felix <esarachof97@gmail.com>
2023-06-02 20:00:11 -0700

    updated Sort3.java
    lab04 Q1

--- lab04/Sort3.java
+++ lab04/Sort3.java
@@ -1,5 +1,7 @@
 package lab04;
 
+import java.util.Scanner;
+
 /**
  * Program name:
  * (program description/purpose)
@@ -14,6 +16,48 @@ public class Sort3 {
      * displays them.
      */
     public static void main(String[] args) {
-
+        int a, b, c; // Declared variables.
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program takes three integer values and sorts them in ascending order.");
+        // Takes input from user.
+        System.out.print("Enter first value: ");
+        a = input.nextInt();
+        System.out.print("Enter second value: ");
+        b = input.nextInt();
+        System.out.print("Enter third value: ");
+        c = input.nextInt();
+        // Sorts and displays in ascending order.
+        if (a < b) {
+            if (a < c) {
+                System.out.println(a);
+                if (b < c) {
+                    System.out.println(b);
+                    System.out.println(c);
+                } else {
+                    System.out.println(c);
+                    System.out.println(b);
+                }
+            } else {
+                System.out.println(c);
+                System.out.println(a);
+                System.out.println(b);
+            }
+        } else {
+            if (b < c) {
+                System.out.println(b);
+                if (a < c) {
+                    System.out.println(a);
+                    System.out.println(c);
+                } else {
+                    System.out.println(c);
+                    System.out.println(a);
+                }
+            } else {
+                System.out.println(c);
+                System.out.println(b);
+                System.out.println(a);
+            }
+        }
+        input.close();
     }
 }
