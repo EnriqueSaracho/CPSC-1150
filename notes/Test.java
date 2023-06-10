2bfcbb702ec7094d64c14d63436f5e71db2b5489
Enrique Saracho Felix <esarachof97@gmail.com>
2023-06-09 20:19:26 -0700

    worked on asgn2 exercise 2

---
+++ notes/Test.java
@@ -0,0 +1,12 @@
+package notes;
+
+public class Test {
+    public static void main(String[] args) {
+        int num = 123;
+        int first, last;
+        first = num % 10;
+        last = num / 100;
+        System.out.println(first);
+        System.out.println(last);
+    }
+}
