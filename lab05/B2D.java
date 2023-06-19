6f32513ccee0ab727574a2594175c06b0479ca86
Enrique Saracho Felix <esarachof97@gmail.com>
2023-06-19 14:34:18 -0700

    worked on lab05
    q1

--- lab05/B2D.java
+++ lab05/B2D.java
@@ -1,3 +1,37 @@
-public class B2D {
+import java.util.Scanner;
 
+/**
+ * Program name: B2D
+ * Converts entered binary number to decimal.
+ * 
+ * @author Enrique Saracho Felix
+ * @since June 19, 2023
+ * 
+ */
+
+public class B2D {
+    /**
+     * main: Prompts user to enter binary number, checks if the number contains a
+     * radix point, then converts it to decimal.
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String binNum;
+        int decNum = 0, count, exp = 0;
+        do {
+            System.out.print("Enter positive binary number: ");
+            binNum = input.nextLine();
+            if (binNum.charAt(0) == '0')
+                System.out.println("Error: invalid input.");
+        } while (binNum.charAt(0) == '0');
+        count = binNum.length() - 1;
+        while (count >= 0) {
+            if (binNum.charAt(count) == '1')
+                decNum += Math.pow(2, exp);
+            count--;
+            exp++;
+        }
+        System.out.printf("Decimal: %d", decNum);
+        input.close();
+    }
 }
