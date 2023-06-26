da34544a66a7bddaf8c0f1eb4f483adda84852c9
Enrique Saracho Felix <esarachof97@gmail.com>
2023-06-26 12:31:42 -0700

    created Test3.java

---
+++ notes/Test3.java
@@ -0,0 +1,9 @@
+public class Test3 {
+    public static void main(String[] args) {
+        int t = 0;
+        if (t > 0)
+            System.out.print("Hello there");
+        else
+            return;
+    }
+}
