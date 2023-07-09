f42367a373767690ff76f4df3929205fa6e2923c
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-07-09 11:48:22 -0700

    asgn4/ArraysExercise.java modified
    worked on asgn4 exercise 1, documentation

--- asgn4/ArraysExercise.java
+++ asgn4/ArraysExercise.java
@@ -1,33 +1,61 @@
 import javax.swing.JOptionPane;
 
+/**
+ * Program name: ArrayExercise
+ * This program can create arrays based on the number of elements and values
+ * specified by the user. It also has methods for displaying it, finding its
+ * maximum and minimum values, the indexes for those values, checking if the
+ * arrays are sorted in ascending or descending order, and more.
+ * 
+ * @author Enrique Saracho Felix
+ * @since July 9, 2023
+ * 
+ */
+
 public class ArraysExercise {
+    /**
+     * main: Calls every method in the program to create and work with arrays.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
-        // int[] array = getArray();
-        // printArray(array);
+        int[] arr1 = getArray(); // Calling getArray().
+        printArray(arr1); // Calling printArray().
 
-        // JOptionPane.showMessageDialog(null, String.format("Maximum: %d",
-        // findMax(array)));
-        // JOptionPane.showMessageDialog(null, String.format("Minimum: %d",
-        // findMin(array)));
-        // JOptionPane.showMessageDialog(null,
-        // String.format("First appearence of Maximum: index %d", findMaxIndex(array)));
-        // JOptionPane.showMessageDialog(null,
-        // String.format("Last appearence of Minimum: index %d", findMinIndex(array)));
+        // Calling findMax().
+        JOptionPane.showMessageDialog(null, String.format("Maximum: %d", findMax(arr1)));
+        // Calling findMin().
+        JOptionPane.showMessageDialog(null, String.format("Minimum: %d", findMin(arr1)));
 
-        // JOptionPane.showMessageDialog(null,
-        // String.format("Array sorted in ascending order: %b", isSortedAscend(array)));
-        // JOptionPane.showMessageDialog(null,
-        // String.format("Array sorted in descending order: %b",
-        // isSortedDescend(array)));
-        // swapNeightbor(array);
-        // JOptionPane.showMessageDialog(null, "After swapNeighbor:");
-        // printArray(array);
+        // Calling findMaxIndex().
+        JOptionPane.showMessageDialog(null, String.format("First appearence of Maximum: index %d", findMaxIndex(arr1)));
+        // Calling findMinIndex().
+        JOptionPane.showMessageDialog(null, String.format("Last appearence of Minimum: index %d", findMinIndex(arr1)));
 
-        int[] array2 = { 2, 6, 9 };
-        int[] array1 = { -1, 5, 11, 12 };
-        printArray(merge(array1, array2));
+        // Calling isSortedAscend().
+        JOptionPane.showMessageDialog(null, String.format("Array sorted in ascending order: %b", isSortedAscend(arr1)));
+
+        // Calling isSortedDescend().
+        JOptionPane.showMessageDialog(null,
+                String.format("Array sorted in descending order: %b", isSortedDescend(arr1)));
+
+        // Calling swapNeighbor().
+        swapNeighbor(arr1);
+        JOptionPane.showMessageDialog(null, "After swapNeighbor:");
+        printArray(arr1);
+
+        // Calling merge().
+        int[] arr2 = { -1, 5, 11, 12 };
+        printArray(arr2);
+        printArray(merge(arr1, arr2));
     }
 
+    /**
+     * getArray: Creates an array by asking the user for the array size, then
+     * initializing it with values entered by the user.
+     * 
+     * @return An array of integers.
+     */
     public static int[] getArray() {
         int n;
         boolean error = false;
@@ -51,6 +79,11 @@ public class ArraysExercise {
         return array;
     }
 
+    /**
+     * printArray: Takes and array as an argument and prints it in tabular format.
+     * 
+     * @param array
+     */
     public static void printArray(int[] array) {
         int n = array.length;
         String elements = "";
@@ -62,6 +95,13 @@ public class ArraysExercise {
         JOptionPane.showMessageDialog(null, String.format("Array:\n%s", elements));
     }
 
+    /**
+     * findMax: Takes an array as an argument and finds its maximum value.
+     * 
+     * @param array
+     * @return An integer representing the element in the array with the maximum
+     *         value.
+     */
     public static int findMax(int[] array) {
         int max = array[0];
 
@@ -73,6 +113,13 @@ public class ArraysExercise {
         return max;
     }
 
+    /**
+     * findMin: Takes an array as an argument and finds its minimum value.
+     * 
+     * @param array
+     * @return An integer representing the element in the array with the minimum
+     *         value.
+     */
     public static int findMin(int[] array) {
         int min = array[0];
 
@@ -84,6 +131,13 @@ public class ArraysExercise {
         return min;
     }
 
+    /**
+     * findMaxIndex: Takes an array as argument and finds the first index at which
+     * the maximum value of the array is.
+     * 
+     * @param array
+     * @return An integer representing an index of the array.
+     */
     public static int findMaxIndex(int[] array) {
         int n = array.length;
         int max = findMax(array);
@@ -96,6 +150,13 @@ public class ArraysExercise {
         return array[n - 1];
     }
 
+    /**
+     * findMaxIndex: Takes an array as argument and finds the last index at which
+     * the minimum value of the array is.
+     * 
+     * @param array
+     * @return An integer representing an index of the array.
+     */
     public static int findMinIndex(int[] array) {
         int n = array.length;
         int min = findMin(array);
@@ -110,6 +171,13 @@ public class ArraysExercise {
         return index;
     }
 
+    /**
+     * isSortedAscend: Takes an array as argument and check whether is sorted in
+     * ascending order or not.
+     * 
+     * @param array
+     * @return A boolean value: 'true' if the array is sorted, 'false' if it's not.
+     */
     public static boolean isSortedAscend(int[] array) {
         int n = array.length;
         int pivot = array[0];
@@ -124,6 +192,13 @@ public class ArraysExercise {
         return true;
     }
 
+    /**
+     * isSortedDescend: Takes an array as argument and check whether is sorted in
+     * descending order or not.
+     * 
+     * @param array
+     * @return A boolean value: 'true' if the array is sorted, 'false' if it's not.
+     */
     public static boolean isSortedDescend(int[] array) {
         int n = array.length;
         int pivot = array[0];
@@ -138,7 +213,13 @@ public class ArraysExercise {
         return true;
     }
 
-    public static void swapNeightbor(int[] array) {
+    /**
+     * swapNeighbor: Takes an array as argument, goes through it swaping the values
+     * if the element on the left is bigger than the element on the right.
+     * 
+     * @param array
+     */
+    public static void swapNeighbor(int[] array) {
         int n = array.length;
 
         for (int i = 0; i < n - 1; i++) {
@@ -150,6 +231,14 @@ public class ArraysExercise {
         }
     }
 
+    /**
+     * merge: Takes two arrays sorted in ascending order and merges them while
+     * keeping the ascending order.
+     * 
+     * @param array1
+     * @param array2
+     * @return An array result of the merger of the two argument arrays.
+     */
     public static int[] merge(int[] array1, int[] array2) {
         if (!isSortedAscend(array1) || !isSortedAscend(array2)) {
             JOptionPane.showMessageDialog(null, "Invalid arguments:\nBoth arrays must be sorted in ascending .",
